7e933465fb16f86e231ebd34a31c8c72ad97c954
Jasmin-Azad <jasminazad22@gmail.com>
2014-07-12 02:52:40 +0600

    initial commit

---
+++ program2.php
@@ -0,0 +1,10 @@
+<?php
+function fruitsName($fname,$colour) {
+echo "$fname. which colour is $colour <br>";
+}
+fruitsName("Apple","red");
+fruitsName("Jackfruit","yellow");
+fruitsName("Green Mango","Green");
+
+
+?>
